7b8c422d0cfe435beac09d414c8d87568a5ac6c8
Martin Virik <MVirik@sk.ibm.com>
2017-02-09 16:13:50 +0100

    use helmet

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var fs = require('fs');
+var helmet = require('helmet');
 
 var app = express();
+
+app.use(helmet());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
