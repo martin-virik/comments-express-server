26a5d019673e1202ab1b0f0c99d3b0ec28be890b
Martin Virik <MVirik@sk.ibm.com>
2017-02-09 15:10:17 +0100

    Routes for getting and posting comments in a global property

--- server.js
+++ server.js
@@ -8,6 +8,22 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
+
+app.use(express.static('public'));
+
+var comments = [
+        {'name': 'Jon', 'email': 'jon@host.com', 'comment': 'Something to say'}
+    ];
+
+app.get('/api/comments', function(req, res) {
+    res.json(comments);
+})
+
+app.post('/api/comments', function(req, res) {
+    comments.unshift(req.body);
+    res.json({'status': 'OK'});
+});
+
 app.listen(3000, function() {
     console.log('Listening to localhost:3000');
 })
